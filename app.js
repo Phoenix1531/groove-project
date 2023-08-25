7257efc09dbaff56431fac25b9569f430a52000c
phoenix1531 <arjun15310@gmail.com>
2023-08-25 22:51:35 +0530

    done

---
+++ app.js
@@ -0,0 +1,8 @@
+var buyBtn = document.getElementById("buy-now")
+
+var text = document.getElementById("thanks-text")
+
+buyBtn.onclick=()=>{
+    text.style.display = "block"
+    buyBtn.style.display = "none"
+}
